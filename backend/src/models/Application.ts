f30be27e5cdcb07eb1daf82699d673b2495a0221
Ulises Salinas <usalinas595@gmail.com>
2024-11-13 14:05:25 -0800

    run format to follow style

--- backend/src/models/Application.ts
+++ backend/src/models/Application.ts
@@ -11,38 +11,38 @@ const applicationStatusSchema = new Schema({
   },
 });
 
-const applicationSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
-  },
-  companyId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-  },
-  companyName: {
-    type: String,
-    required: true,
-  },
-  position: {
-    type: String,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: false,
-  },
-  progress: {
-    type: [applicationStatusSchema],
-    default: [],
-  },
+const applicationSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    companyId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: false,
+    },
+    progress: {
+      type: [applicationStatusSchema],
+      default: [],
+    },
   },
   {
-    timestamps: true
-  }
+    timestamps: true,
+  },
 );
 
 type Application = InferSchemaType<typeof applicationSchema>;
 
 export default model<Application>("Application", applicationSchema);
-
