183cab188ec5f79ed9da179ee069a4f044342395
widjaja <wwidjaja0@gmail.com>
2024-11-12 17:05:39 -0800

    ran prettier to resolve styling issues

--- backend/src/models/Company.ts
+++ backend/src/models/Company.ts
@@ -1,29 +1,27 @@
 import { InferSchemaType, Schema, model } from "mongoose";
 
-const companySchema = new Schema(
-  {
-    _id: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    name: {
-      type: String,
-      trim: true,
-      required: true,
-    },
-    city: {
-      type: String,
-      trim: true,
-      required: false,
-    },
-    state: {
-      type: String,
-      trim: true,
-      required: false,
-    },
+const companySchema = new Schema({
+  _id: {
+    type: String,
+    unique: true,
+    required: true,
   },
-);
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  city: {
+    type: String,
+    trim: true,
+    required: false,
+  },
+  state: {
+    type: String,
+    trim: true,
+    required: false,
+  },
+});
 
 type Company = InferSchemaType<typeof companySchema>;
 
