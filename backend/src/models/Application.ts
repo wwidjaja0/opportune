22283af9e222d4923635cad3a1ffc2f564696f77
Ulises Salinas <usalinas595@gmail.com>
2024-11-13 00:51:07 -0800

    ran lint to format my code up to standards

---
+++ backend/src/models/Application.ts
@@ -0,0 +1,44 @@
+import { InferSchemaType, Schema, model } from "mongoose";
+
+const applicationSchema = new Schema({
+  _id: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  companyId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  companyName: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: false,
+  },
+  progress: {
+    type: [String],
+    default: [],
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+  dateModified: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+type Application = InferSchemaType<typeof applicationSchema>;
+export default model<Application>("Application", applicationSchema);
