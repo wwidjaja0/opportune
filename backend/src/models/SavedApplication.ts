eda129939d6225874f895045fbf418d1f3b88ff0
yxli001 <lxd520anna@gmail.com>
2024-11-15 23:41:35 -0800

    Add timestamps to SavedApplication model

--- backend/src/models/SavedApplication.ts
+++ backend/src/models/SavedApplication.ts
@@ -1,35 +1,41 @@
 import { InferSchemaType, Schema, model } from "mongoose";
 
-const savedApplicationSchema = new Schema({
-  userId: {
-    type: String,
-    required: true,
+const savedApplicationSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    companyId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: false,
+    },
+    materialsNeeded: {
+      type: [String],
+      default: [],
+    },
+    deadline: {
+      type: Date,
+      required: false,
+    },
   },
-  companyId: {
-    type: Schema.Types.ObjectId,
-    required: true,
+  {
+    // Automatically generates created and updated times
+    timestamps: true,
   },
-  companyName: {
-    type: String,
-    required: true,
-  },
-  position: {
-    type: String,
-    required: true,
-  },
-  link: {
-    type: String,
-    required: false,
-  },
-  materialsNeeded: {
-    type: [String],
-    default: [],
-  },
-  deadline: {
-    type: Date,
-    required: false,
-  },
-});
+);
 
 type SavedApplication = InferSchemaType<typeof savedApplicationSchema>;
 
