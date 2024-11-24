1d7f011b3bf97a34c057430b032857bd57f88fbf
Ulises Salinas <usalinas595@gmail.com>
2024-11-24 13:59:51 -0800

    update application's controller comments, created skeleton validator for application, and updated controller functions to not use ts following general style

--- backend/src/routes/applicationRoutes.ts
+++ backend/src/routes/applicationRoutes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import * as applicationController from "src/controllers/applicationController";
-
+import * as applicationValidator from "src/validators/applicationValidator";
 const applicationRouter = express.Router();
 
 applicationRouter.get("/", applicationController.getAllApplications);
 applicationRouter.post("/", applicationController.createApplication);
 applicationRouter.get("/:id", applicationController.getApplicationByID);
-applicationRouter.patch("/:id", applicationController.updateApplicationID);
+applicationRouter.patch("/:id", applicationController.updateApplicationByID);
 applicationRouter.delete("/:id", applicationController.deleteApplicationByID);
 applicationRouter.get(
   "/user/:id",
---
+++ backend/src/validators/applicationValidator.ts
@@ -0,0 +1,4 @@
+import { body } from "express-validator";
+
+export const createApplicationValidator = {};
+export const updateApplicationValidator = {};
